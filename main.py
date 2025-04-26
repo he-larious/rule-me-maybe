9f912d08b0ec7a906e791bebcc6a0f2b886a2463
Kristine Pham <kristineph101@gmail.com>
2025-04-26 18:54:59 -0400

    update csv

--- main.py
+++ main.py
@@ -199,7 +199,6 @@ def main():
         for rule in high_conf_rules:
             f.write(f"[{','.join(rule[0])}] => [{rule[1]}] (Conf: {round(rule[2]*100, 4)}%, Supp: {round(rule[3]*100, 4)}%)\n")
 
-
 if __name__ == "__main__":
     main()
 
@@ -233,9 +232,13 @@ def update_csv(csv_file):
     labels = ['18-25', '26-35', '36-45', '46-55', '56-65', '66-75', '76-85', '86-95']
     df['AGE_RANGE'] = pd.cut(df['AGE'], bins=bins, labels=labels, include_lowest=True)
 
+    # replace admission dates with months
+    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    df['ADMITTED_DT'] = df['ADMITTED_DT'].apply(lambda x: months[int(x.split('/')[0])-1])
+
     df = df.drop(columns=["GENDER", "INMATEID", "DISCHARGED_DT", "SEALED", "AGE"])
 
-    df.to_csv("daily_inmates_no_gender.csv", index=False)
+    df.to_csv("daily_inmates_final.csv", index=False)
 '''
 
 
